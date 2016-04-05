dbe13a2e58411101a7b046b4c8aebef332a630c8
2013411 <s1186093704@gmail.com>
2016-04-05 23:37:50 +0800

    task 29

---
+++ js/task29.js
@@ -0,0 +1,17 @@
+/**
+ *任务表单验证
+ * 
+ */
+
+
+window.onload=function(){
+	var oinput=$('.oinput');
+	var obtn=$('.obtn');
+	var otext=$('.otext');
+
+	function valid(){
+		this.regu="/[0-9a-zA-Z]/"
+	}
+
+
+}
