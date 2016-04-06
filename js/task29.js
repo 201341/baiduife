198f4793366b4044ad480a027e7f4e0dca2cacbb
2013411 <s1186093704@gmail.com>
2016-04-06 16:21:04 +0800

    task 29999

--- js/task29.js
+++ js/task29.js
@@ -4,14 +4,42 @@
  */
 
 
-window.onload=function(){
+(function(){
 	var oinput=$('.oinput');
 	var obtn=$('.obtn');
 	var otext=$('.otext');
+ 	
+ 	
+ 	function valide(){
+ 		var inputvalue=oinput.value.replace(/[\u4e00-\u9fa5]/g,"aa");//用两个英文代替中文
+ 		var regu=/^[0-9a-zA-Z]{4,16}$/;
+ 		if(!inputvalue){
+ 			otext.innerHTML="姓名不能为空";
+ 			otext.style.color="red";
+ 			oinput.style.border="2px solid red";
 
-	function valid(){
-		this.regu="/[0-9a-zA-Z]/"
-	}
+ 			
+ 		}else if(!regu.test(trim(inputvalue))){
+ 			otext.innerHTML="字符串长度不对";
+ 			otext.style.color="red";
+ 			oinput.style.border="2px solid red";
 
 
-}
+ 		}
+ 		else{
+
+ 			otext.innerHTML="格式正确";
+ 			otext.style.color="lightgreen";
+ 			oinput.style.border="2px solid lightgreen";
+
+
+
+ 		}
+
+ 	}
+
+
+
+ 	addhandler(obtn,"click",valide);
+
+})();
